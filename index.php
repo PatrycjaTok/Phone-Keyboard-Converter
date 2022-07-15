28f844951ff9a0b6cc86f8f2ad422f52a2bd52d1
PatrycjaTok <patrycjasuper999@gmail.com>
2022-07-15 16:56:56 +0200

    create repository and empty php file

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="pl-PL">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale-=1">
+        <link rel="stylesheet" href="css/style_editdelete.css" type="text/css">
+        <link rel="icon" href="images/favicon.png" type="image/x-icon">
+        <title>PHONE KEYBOARD CONVERTER</title>
+    </head>
+    <body>
+        <div class="container">
+
+        </div>
+        
+    </body>
+
+
+</html>
